cc6b07a621817e5ddd93da2d2e826cc8a1124bba
Ghala <Aghala937@gmail.com>
2024-08-07 19:27:06 +0300

    added pages

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Button from "$lib/components/ui/button/button.svelte";
-    import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
-</script>
-
-<div class="flex justify-center min-h-screen items-center  ">
-   <ThemeSwitch/> <Button/>
-   <div class="bg-red-500 h-10 w-10 border-4">  </div>
-</div>
